2027e11e9c680a4360850e1ee3841d8e9028d69b
Anton Butov <butov6101@gmail.com>
2020-04-17 13:43:01 +0300

    Первый запуск.

---
+++ app/src/main/java/pro/butovanton/instaforex_2/IReciclerAdapter.java
@@ -0,0 +1,61 @@
+package pro.butovanton.instaforex_2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class IReciclerAdapter extends RecyclerView.Adapter<IReciclerAdapter.IViewHolder> {
+
+    private Context context;
+    private List<Signal> signals;
+
+    public IReciclerAdapter(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public IViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View rootView = LayoutInflater.from(context).inflate(R.layout.recyclerview_item, parent, false);
+        return new IViewHolder(rootView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull IViewHolder holder, int position) {
+        Signal signal = signals.get(position);
+        holder.textView.setText(signal.pair + " : " + signal.comment);
+    }
+
+    void setSignals(List<Signal> signals){
+        this.signals = signals;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if (signals != null) {
+            return signals.size();
+        } else {
+            return 0;
+        }
+
+    }
+
+
+    public class IViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textView;
+
+        public IViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+    }
+}
--- app/src/main/java/pro/butovanton/instaforex_2/MainActivity.java
+++ app/src/main/java/pro/butovanton/instaforex_2/MainActivity.java
@@ -3,6 +3,8 @@ package pro.butovanton.instaforex_2;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,13 +13,18 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final int LOGIN_ACTIVITY_REQUEST_CODE = 1;
-
+    private RecyclerView recyclerView;
     private IViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final IReciclerAdapter adapter = new IReciclerAdapter(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
 
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivityForResult(intent, LOGIN_ACTIVITY_REQUEST_CODE);
